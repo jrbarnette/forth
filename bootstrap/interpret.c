6b51f720bb0c622bff6f4010bb03eda2a4df408a
Richard Barnette <rbarnette@acm.org>
2019-03-23 11:50:42 -0700

    Small refactoring in meta-mode functions.
    Name lookup and error reporting were the same in meta_interpret()
    and meta_compile(), so factored it out into a static function.

--- bootstrap/interpret.c
+++ bootstrap/interpret.c
@@ -299,22 +299,27 @@ interpret_string(vmstate_p vm, char *s)
 }
 
 
+static xt_ft
+meta_lookup(vmstate_p vm, vminstr_p ip)
+{
+    name_p name = lookup(vm, (c_addr_ft) ip->id, strlen(ip->id));
+    if (name == NULL) {
+	/*
+	 * XXX ptrdiff_t wants %ld for 64-bit systems, but this
+	 * could cause warnings on 32-bit systems.
+	 */
+	fprintf(stderr, "name '%s' not found at offset %ld\n",
+		ip->id, ip - initialize_forth);
+	abort();
+    }
+    return NAME_XT(name);
+}
+
 vminstr_p
 meta_interpret(vminstr_p ip, vmstate_p vm, vmarg_p ignore)
 {
     while (ip->id != NULL) {
-        name_p name = lookup(vm, (c_addr_ft) ip->id, strlen(ip->id));
-	if (name == NULL) {
-            /*
-             * XXX ptrdiff_t wants %ld for 64-bit systems, but this
-             * could cause warnings on 32-bit systems.
-             */
-	    fprintf(stderr, "name '%s' not found at offset %ld\n",
-                    ip->id, ip - initialize_forth);
-	    abort();
-	}
-	xt_ft xtok = NAME_XT(name);
-	execute(vm, xtok);
+	execute(vm, meta_lookup(vm, ip));
 	ip++;
     }
     return ip + 1;
@@ -325,18 +330,7 @@ vminstr_p
 meta_compile(vminstr_p ip, vmstate_p vm, vmarg_p ignore)
 {
     while (ip->id != NULL) {
-        name_p name = lookup(vm, (c_addr_ft) ip->id, strlen(ip->id));
-	if (name == NULL) {
-            /*
-             * XXX ptrdiff_t wants %ld for 64-bit systems, but this
-             * could cause warnings on 32-bit systems.
-             */
-	    fprintf(stderr, "name '%s' not found at offset %ld\n",
-                    ip->id, ip - initialize_forth);
-	    abort();
-	}
-	xt_ft xtok = NAME_XT(name);
-	COMPILE(vm, xtok);
+	COMPILE(vm, meta_lookup(vm, ip));
 	ip++;
     }
     return ip + 1;

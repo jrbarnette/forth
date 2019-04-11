04eba46b99436f7b669dd65589248b785c1e30a8
Richard Barnette <rbarnette@acm.org>
2019-04-11 12:53:58 -0700

    Move selected C primitives from "interpret" to "vm"

--- bootstrap/interpret.c
+++ bootstrap/interpret.c
@@ -20,29 +20,6 @@
 //------  ------  ------  ------  ------  ------  ------  ------
 
 
-/* ( -- x ) runtime semantics */
-vminstr_p
-do_literal(vminstr_p ip, vmstate_p vm, vmarg_p ignore)
-{
-    CHECK_PUSH(vm, 1);
-    PUSH(vm, ip->cell);
-    return ip + 1;
-}
-
-
-/* ( -- c-addr u ) runtime semantics */
-vminstr_p
-do_s_quote(vminstr_p ip, vmstate_p vm, vmarg_p ignore)
-{
-    cell_ft len = ip->cell;
-
-    CHECK_PUSH(vm, 2);
-    PUSH(vm, ip + 1);
-    PUSH(vm, len);
-    return (vminstr_p) ((cell_ft) ip + XALIGNED(len + CELL_SIZE));
-}
-
-
 /* ( -- ) compilation semantics */
 vminstr_p
 x_left_bracket(vminstr_p ip, vmstate_p vm, vmarg_p ignore)
@@ -58,16 +35,4 @@ x_right_bracket(vminstr_p ip, vmstate_p vm, vmarg_p ignore)
 {
     DICT.state = STATE_COMPILE;
     return ip;
-}
-
-
-/* ( -- c-addr ) runtime semantics */
-vminstr_p
-do_c_quote(vminstr_p ip, vmstate_p vm, vmarg_p ignore)
-{
-    cell_ft len = ip->cdata[0];
-
-    CHECK_PUSH(vm, 1);
-    PUSH(vm, &ip->cdata[0]);
-    return (vminstr_p) ((cell_ft) ip + XALIGNED(len + 1));
 }
--- bootstrap/vm.c
+++ bootstrap/vm.c
@@ -15,18 +15,22 @@
 
 // Contains all semantics for the following standard definitions:
 //------  ------  ------  ------  ------  ------  ------  ------
-// EXECUTE               6.1.1370 CORE                   38
-// EXIT                  6.1.1380 CORE                   38
+// EXECUTE               6.1.1370 CORE
+// EXIT                  6.1.1380 CORE
 //
-// THROW               9.6.1.2275 EXCEPTION              73
+// THROW               9.6.1.2275 EXCEPTION
 //------  ------  ------  ------  ------  ------  ------  ------
 
 // Contains runtime or other partial semantics for the following:
 //------  ------  ------  ------  ------  ------  ------  ------
-// :                     6.1.0450 CORE                   30
-// CONSTANT              6.1.0950 CORE                   35
-// CREATE                6.1.1000 CORE                   36
-// VARIABLE              6.1.2410 CORE                   47
+// :                     6.1.0450 CORE
+// CONSTANT              6.1.0950 CORE
+// CREATE                6.1.1000 CORE
+// LITERAL               6.1.1780 CORE
+// S"                    6.1.2165 CORE
+// VARIABLE              6.1.2410 CORE
+//
+// C"                    6.2.0855 CORE EXT
 //------  ------  ------  ------  ------  ------  ------  ------
 
 
@@ -124,6 +128,41 @@ quit(vmstate_p vm, FILE *input)
 
 
 /* -------------------------------------------------------------- */
+
+/* ( -- x ) runtime semantics */
+vminstr_p
+do_literal(vminstr_p ip, vmstate_p vm, vmarg_p ignore)
+{
+    CHECK_PUSH(vm, 1);
+    PUSH(vm, ip->cell);
+    return ip + 1;
+}
+
+
+/* ( -- c-addr u ) runtime semantics */
+vminstr_p
+do_s_quote(vminstr_p ip, vmstate_p vm, vmarg_p ignore)
+{
+    cell_ft len = ip->cell;
+
+    CHECK_PUSH(vm, 2);
+    PUSH(vm, ip + 1);
+    PUSH(vm, len);
+    return (vminstr_p) ((cell_ft) ip + XALIGNED(len + CELL_SIZE));
+}
+
+
+/* ( -- c-addr ) runtime semantics */
+vminstr_p
+do_c_quote(vminstr_p ip, vmstate_p vm, vmarg_p ignore)
+{
+    cell_ft len = ip->cdata[0];
+
+    CHECK_PUSH(vm, 1);
+    PUSH(vm, &ip->cdata[0]);
+    return (vminstr_p) ((cell_ft) ip + XALIGNED(len + 1));
+}
+
 
 /* ( R: -- nest-sys ) initiation semantics */
 vminstr_p

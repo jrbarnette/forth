dcc382e2b0a5261bfc30bfb62a4b481d45a0bae2
Richard Barnette <rbarnette@acm.org>
2020-12-28 17:24:56 -0800

    Add an explanatory block comment to forth.h.

--- bootstrap/forth.h
+++ bootstrap/forth.h
@@ -5,6 +5,18 @@
 #ifndef FORTH_H
 #define FORTH_H
 
+/*
+ * The headers below aren't meant to be included independently:
+ * none of them include other headers that they might depend on.
+ * In the list below, every header depends on the headers that
+ * precede, except that "prim.h" doesn't depend on "vmcheck.h".
+ *
+ * All of the content included here is needed by virtually every
+ * XT primitive handler, so the content isn't really independently
+ * useful.  The pieces are split apart mostly to keep any one file
+ * from being too big.
+ */
+
 #include "ftypes.h"
 #include "vmstate.h"
 #include "xtoken.h"
